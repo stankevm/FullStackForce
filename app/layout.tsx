a2251a9124a7349c45877e92117448ba9a90312c
Marta Stankevich <nkvchm@gmail.com>
2025-07-16 18:05:01 +0200

    Refactor button styles in globals.css for improved aesthetics; update layout to include new font preconnects; enhance RocketLaunchAnimation with dynamic typing phases and code highlighting for better user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Funnel+Display:wght@300..800&family=Poppins:wght@100..900&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body className={inter.className}>{children}</body>
